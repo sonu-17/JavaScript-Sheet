1a392b062a386a7395e5f891ad8ff44f467964c7
sonu-17 <102901950+sonu-17@users.noreply.github.com>
2023-10-13 19:54:20 +0530

    Day02 Hard

---
+++ Day02/hard/Answer1.js
@@ -0,0 +1,3 @@
+ //"love is the best thing in the world.some found their love and some are still looking for their love".Count the number of words love in the sentence.//
+let string = "love is the best thing in the world.Some found there love and some are still looking for their love"
+console.log((string.search("love")))
---
+++ Day02/hard/Answer2.js
@@ -0,0 +1,4 @@
+2.//use match() to count the number of all because in the following sentence:"you cannot end a sentence with because because because is a conjunction"//
+
+let sentence2 ="You can not end a sentence with because because because is conjuction"
+console.log(sentence2.match(/'because'/gi).length)
